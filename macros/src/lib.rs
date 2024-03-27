cc8d0ae7d16b854b0d9f5e087cfc56468b61e3bf
Ryan Tate <ryan.michael.tate@gmail.com>
2024-03-27 09:00:51 -0700

    update proc macro attributes
    Signed-off-by: Ryan Tate <ryan.michael.tate@gmail.com>

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -3,14 +3,22 @@ use docbuf_derive::{derive, proc_macro2, quote::quote};
 use proc_macro::TokenStream;
 
 #[proc_macro_derive(DocBuf)]
-pub fn derive_docbuf(_input: TokenStream) -> TokenStream {
-    // let attr = proc_macro2::TokenStream::from(TokenStream::new());
-    // let input = proc_macro2::TokenStream::from(input);
+pub fn derive_docbuf(item: TokenStream) -> TokenStream {
+    let attr = proc_macro2::TokenStream::from(TokenStream::new());
+    let item = proc_macro2::TokenStream::from(item);
 
-    // let impl_docbuf = derive::docbuf_impl(attr, input);
+    let name = derive::parse_item_name(&item);
+    let lifetimes = derive::parse_item_lifetimes(&item);
+    let options = derive::DocBufOpts::from(&attr);
+
+    let docbuf_methods = derive::docbuf_impl(&name, &lifetimes, &options, &item);
+    let crypto_methods = derive::docbuf_impl_crypto(&name, &lifetimes, &options);
+    let db_methods = derive::docbuf_impl_db(&name, &lifetimes, &options, &item);
 
     quote! {
-        // #impl_docbuf
+        #docbuf_methods
+        #crypto_methods
+        #db_methods
     }
     .into()
 }
